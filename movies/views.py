c71785f0d57febb703de0cd04617d9299d9d1c41
logan-tolbert <logo88205+git@gmail.com>
2024-03-23 12:21:43 -0500

    Initial commit

---
+++ movies/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    context = {
+        'movies': ['The Fifth Element', 'Fear and Loathing in Las Vegas', 'The Wall', 'Casino']
+    }
+    return render(request, 'movies/index.html', context)
+
+
+def about(request):
+    return render(request, 'movies/about.html', {})
